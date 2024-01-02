54c1f79947ae2226c2dd003bbe5bac7cc36b47cf
Isaiah19 <nwezeifeanyi93@gmail.com>
2024-01-02 15:43:22 -0800

    133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,13 +1,12 @@
 #include "binary_trees.h"
 
 /**
-* heap_extract - function that extracts the root node of a Max Binary Heap
-* @root: double pointer to the root node of heap
-* Return: value stored in the root node
-*/
-
+ * heap_to_sorted_array - converts a Binary Max Heap to a sorted array of integers
+ *
+ * @root: double pointer to the root node of heap
+ * Return: value stored in the root node
+ */
 int heap_extract(heap_t **root)
 {
-	printf("%d", (*root)->n);
-	return (0);
+	return (root);
 }
